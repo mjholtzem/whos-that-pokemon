8953b6d688307520f41355fc57fb75d605c32707
Matthew Holtzem <mjholtzem@gmail.com>
2026-01-09 17:18:04 -0700

    adding Vercel analytics

--- whos-that-pokemon/src/main.tsx
+++ whos-that-pokemon/src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { PokemonProvider } from "./contexts/PokemonContext.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics/>
     <PokemonProvider>
       <App />
     </PokemonProvider>
